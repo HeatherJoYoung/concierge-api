53c27c5b33f4c8b8b08af5cadd56492a8f966838
Heather Young <hjy.earth@gmail.com>
2023-09-27 16:53:16 -0600

    added src directory

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const userRoutes = require('./routes/user.route');
+const userRoutes = require('./src/routes/user.route');
 
 app.use('/users', userRoutes);
 
