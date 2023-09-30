5ae3ed136ea5fae8f32d03ae8352ff9976d0d39d
omar <o_omer@mail.fhsu.edu>
2023-09-30 16:01:57 +0300

    Added spa routes to app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const userRoutes = require('./src/routes/user.route');
+const spaRoutes = require('./src/routes/spa.route');
 
 app.use('/users', userRoutes);
+app.use('/spa/services', spaRoutes)
 
 app.use(express.json());
 
