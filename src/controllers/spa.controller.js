51b0d6cfcb185cabdd0a4f7a22018861f8354ffa
omar <o_omer@mail.fhsu.edu>
2023-10-07 02:21:12 +0300

    Created create service and delete service functions in spa controller

--- src/controllers/spa.controller.js
+++ src/controllers/spa.controller.js
@@ -1,6 +1,8 @@
+const { param } = require('../routes/user.route');
 const spaService = require('../services/spa.service');
 
-exports.getAllServices = (req, res, next) => {
+
+exports.getAllServices = (req, res) => {
     console.log('in services controller')
     spaService.getAllServices((error, results) => {
         if (error) {
@@ -12,4 +14,56 @@ exports.getAllServices = (req, res, next) => {
             data: results.recordsets[0]
         });
     });
+}
+
+exports.createService =  async (req, res) => {
+    console.log("Create service controller");
+
+    const { title, descript } = req.body;
+
+    if (!title || !descript) {
+        return res.status(400).json({
+          status: 0,
+          message: "Both 'title' and 'descript' are required fields.",
+        });
+    }
+
+    spaService.createService(title, descript, (error, result) => {
+        if(error){
+           return res.status(400).json({
+                status: 0,
+                message: err.message
+            });
+        }
+        return res.status(200).json({
+            status: 1,
+            message: result.recordsets[0]
+        })
+    })
+};
+
+
+exports.deleteService =  (req, res) => {
+
+    const id = req.body.id;
+
+    if(!id){
+        return res.status(400).json({
+            status: 0,
+            message: "'id' is required and must be an integer"
+        })
+    }
+
+    spaService.deleteService(id, (error, result) => {
+        if(error) {
+            return res.status(400).json({
+                status: 0,
+                message: error.message
+            });
+        }
+        return res.status(201).json({
+            status: 1,
+            message: "Record deleted"
+        })
+    })
 }
