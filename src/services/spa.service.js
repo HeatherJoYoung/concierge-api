25b2e707554527696a2417f29992d1f35c83bc49
omar <o_omer@mail.fhsu.edu>
2023-10-07 02:24:13 +0300

    Created the delete and create services

--- src/services/spa.service.js
+++ src/services/spa.service.js
@@ -1,15 +1,18 @@
 // const db = require("../dbconnection");
+const { request } = require('express');
 const sql = require('mssql');
+
+
+const config = {
+    user: 'sa',
+    password: 'sa',
+    server: 'localhost',
+    database: 'Demo',
+    trustServerCertificate: true
+}
 
 exports.getAllServices = (callback) => {
     console.log('in spa services');
-    const config = {
-        user: 'sa',
-        password: 'sa',
-        server: 'localhost',
-        database: 'Demo',
-        trustServerCertificate: true
-    }
 
     sql.connect(config, (err) => {
         if (err) {
@@ -18,6 +21,40 @@ exports.getAllServices = (callback) => {
         const request = new sql.Request();
         request.query('select * from spa_services', (err, data) => {
             return callback(err, data);
+        });
+    })
+
+}
+
+exports.createService =  (title, descript, callback) => {
+    sql.connect(config, (err) => {
+        if(err){
+            return callback(err);
+        }
+        const request = new sql.Request();
+        request.input('title', sql.VarChar(30), title);
+        request.input('descript', sql.VarChar(200), descript);
+
+        request.query(
+            'INSERT INTO spa_services (title, descript) VALUES (@title, @descript);', (err, data) => {
+                return callback(err, data);
+            });
+    })
+};
+  
+
+exports.deleteService = (id, callback) => {
+
+    console.log(id);
+    sql.connect(config, (err) => {
+        if(err){
+            return callback(err);
+        }
+        const request = new sql.Request();
+        request.input('id', sql.Int, id);
+        request.query(
+            'DELETE FROM spa_services WHERE id = @id', (err, data) => {
+                return callback(err, data);
         });
     })
 }
