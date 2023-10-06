b0540cd0e44f982f8d38be0d3e852bcbd7af52d9
omar <o_omer@mail.fhsu.edu>
2023-10-07 02:22:39 +0300

    changed the url from spa/service/all to spa/services for better consistency

--- src/routes/spa.route.js
+++ src/routes/spa.route.js
@@ -1,6 +1,9 @@
-const userController = require('../controllers/spa.controller');
+const spaCreateController = require('../controllers/spa.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/all', userController.getAllServices);
+router.get('/', spaCreateController.getAllServices);
+router.post('/', spaCreateController.createService);
+router.delete('/', spaCreateController.deleteService);
+
 module.exports = router;
