5de2128c1967fb98a120fd2c3215914f3d3a6b95
omar <o_omer@mail.fhsu.edu>
2023-10-22 19:43:16 +0300

    added app.js

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const userRoutes = require('./src/routes/user.route');
 const spaRoutes = require('./src/routes/spa.route');
 const diningRoutes = require('./src/routes/dining.route');
 const eventsRoutes = require('./src/routes/events.route');
+const reservationRoutes = require('./src/routes/reservation.route')
+
+app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/spa/services', spaRoutes);
 app.use('/events', eventsRoutes);
 app.use('/dining', diningRoutes);
-
-app.use(express.json());
+app.use('/reservation', reservationRoutes)
 
 app.listen(port, () => {
     console.log('Server listening on PORT:', port)
